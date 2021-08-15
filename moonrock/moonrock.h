2a0feb3caeeb06b5767e634ae9969aff0e21c005
Sungmin Woo <woos8899@gmail.com>
2021-08-15 18:50:31 +0900

    Basic rasterizer

--- moonrock/moonrock.h
+++ moonrock/moonrock.h
@@ -7,6 +7,45 @@
 namespace moonrock {
 
     template <typename T>
+    class tvec2 {
+
+    public:
+        T x = 0;
+        T y = 0;
+
+    public:
+        tvec2<T> operator+(tvec2<T> other) const {
+            return tvec2<T>{ this->x + other.x, this->y + other.y };
+        }
+
+        tvec2<T> operator-(tvec2<T> other) const {
+            return tvec2<T>{ this->x - other.x, this->y - other.y };
+        }
+
+        tvec2<T> rotate_cw_90() const {
+            return tvec2<T>{ y, -x };
+        }
+
+        tvec2<T> rotate_ccw_90() const {
+            return tvec2<T>{ -y, x };
+        }
+
+        auto dot(tvec2<T> other) const {
+            return this->x * other.x + this->y * other.y;
+        }
+
+    private:
+        friend std::ostream& operator<<(std::ostream& os, const moonrock::tvec2<T>& obj) {
+            os << "{" << obj.x << ", " << obj.y << "}";
+            return os;
+        }
+
+    };
+
+    using vec2 = tvec2<float>;
+
+
+    template <typename T>
     class tvec4 {
 
     public:
@@ -22,6 +61,8 @@ namespace moonrock {
         }
 
     };
+
+    using vec4 = tvec4<float>;
 
 
     class Pixel4Uint8 : protected tvec4<uint8_t> {
@@ -101,10 +142,10 @@ namespace moonrock {
 
     private:
         std::vector<_PixelTyp> m_data;
-        uint64_t m_width, m_height;
+        uint32_t m_width, m_height;
 
     public:
-        Image2D(const uint64_t width, const uint64_t height)
+        Image2D(const uint32_t width, const uint32_t height)
             : m_data(width * height)
             , m_width(width)
             , m_height(height)
@@ -124,16 +165,22 @@ namespace moonrock {
             return this->m_data.data();
         }
 
-        auto& pixel(const uint64_t x, const uint64_t y) {
+        auto& pixel(const uint32_t x, const uint32_t y) {
             return this->m_data[this->calc_index(x, y)];
         }
 
-        auto& pixel(const uint64_t x, const uint64_t y) const {
+        auto& pixel(const uint32_t x, const uint32_t y) const {
             return this->m_data[this->calc_index(x, y)];
+        }
+
+        void clear(const float x, const float y, const float z, const float w) {
+            for (auto& pixel : this->m_data) {
+                pixel.set_color_xyzw(x, y, z, w);
+            }
         }
 
     private:
-        size_t calc_index(const uint64_t x, const uint64_t y) const {
+        size_t calc_index(const uint32_t x, const uint32_t y) const {
             return x + y * this->width();
         }
 
@@ -144,5 +191,80 @@ namespace moonrock {
 
 
     bool export_image_to_disk(const char* const output_path, const ImageUint2D& img);
+
+
+    class Rasterizer {
+
+    public:
+        std::array<vec2, 3> m_vertices;
+        uint32_t domain_width = 0, domain_height = 0;
+
+    public:
+        std::pair<vec2, vec2> make_min_max() const {
+            vec2 min = this->m_vertices[0];
+            vec2 max = this->m_vertices[0];
+
+            for (size_t i = 1; i < this->m_vertices.size(); ++i) {
+                const auto& v = this->m_vertices[i];
+
+                if (v.x < min.x)
+                    min.x = v.x;
+                if (v.y < min.y)
+                    min.y = v.y;
+
+                if (v.x > max.x)
+                    max.x = v.x;
+                if (v.y > max.y)
+                    max.y = v.y;
+            }
+
+            return std::make_pair(min, max);
+        }
+
+        bool is_pix_inside(const float x, const float y) const {
+            return this->PointInTriangle(vec2{ x, y }, this->m_vertices[0], this->m_vertices[1], this->m_vertices[2]);
+        }
+
+        void work(std::vector<tvec2<uint32_t>>& output) const {
+            output.clear();
+
+            const auto [min, max] = this->make_min_max();
+
+            for (uint32_t x = min.x; x <= max.x; ++x) {
+                for (uint32_t y = min.y; y < max.y; ++y) {
+                    if (this->is_pix_inside(x, y)) {
+                        output.push_back(tvec2<uint32_t>{x, y});
+                    }
+                }
+            }
+        }
+
+        std::vector<tvec2<uint32_t>> work() const {
+            std::vector<tvec2<uint32_t>> output;
+            this->work(output);
+            return output;
+        }
+
+    private:
+        // https://stackoverflow.com/a/2049593/6819534
+        static float sign(vec2 p1, vec2 p2, vec2 p3) {
+            return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+        }
+
+        static bool PointInTriangle(vec2 pt, vec2 v1, vec2 v2, vec2 v3) {
+            float d1, d2, d3;
+            bool has_neg, has_pos;
+
+            d1 = sign(pt, v1, v2);
+            d2 = sign(pt, v2, v3);
+            d3 = sign(pt, v3, v1);
+
+            has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+            has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+            return !(has_neg && has_pos);
+        }
+
+    };
 
 }
--- test/output_img.cpp
+++ test/output_img.cpp
@@ -4,11 +4,28 @@
 
 
 int main() {
-    moonrock::ImageUint2D img{ 32, 32 };
+    moonrock::ImageUint2D img{ 512, 512 };
+    img.clear(1, 1, 1, 1);
 
-    for (int i = 0; i < img.width(); ++i) {
-        auto& pix = img.pixel(i, 0);
-        pix.set_color_xyzw(123, 14, 0, 12);
+    moonrock::Rasterizer rasterizer;
+    rasterizer.domain_width = img.width();
+    rasterizer.domain_height = img.height();
+    rasterizer.m_vertices[0] = moonrock::vec2{200, 30};
+    rasterizer.m_vertices[1] = moonrock::vec2{120, 300};
+    rasterizer.m_vertices[2] = moonrock::vec2{420, 80};
+
+    for (auto v : rasterizer.work()) {
+        img.pixel(v.x, v.y).set_color_y(0);
+        img.pixel(v.x, v.y).set_color_z(0);
+    }
+
+    rasterizer.m_vertices[0] = moonrock::vec2{420, 80};
+    rasterizer.m_vertices[1] = moonrock::vec2{120, 300};
+    rasterizer.m_vertices[2] = moonrock::vec2{480, 500};
+
+    for (auto v : rasterizer.work()) {
+        img.pixel(v.x, v.y).set_color_x(0);
+        img.pixel(v.x, v.y).set_color_y(0);
     }
 
     std::cout << moonrock::export_image_to_disk("C:\\Users\\woos8\\Downloads\\output.png", img) << std::endl;
