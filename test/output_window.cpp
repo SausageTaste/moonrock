512084259738cb24aeb7efec77e489aa281ec65f
Sungmin Woo <woos8899@gmail.com>
2021-12-07 21:21:51 +0900

    Use SDL for window

--- test/output_window.cpp
+++ test/output_window.cpp
@@ -1,313 +1,57 @@
-#include <chrono>
-#include <thread>
-#include <cassert>
-#include <iostream>
-#include <exception>
-
-#include <glad/glad.h>
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-
-
-#include "render_things.h"
-
-
-// GLFW
-namespace {
-
-    constexpr unsigned int MIN_WIN_WIDTH = 128;
-    constexpr unsigned int MIN_WIN_HEIGHT = 128;
-
-
-    void callback_error(int error, const char* description) {
-        std::cout << description << std::endl;
-    }
-
-    void callback_resize_fbuf(GLFWwindow* const window, int width, int height) {
-        glViewport(0, 0, width, height);
-    }
-
-
-    class WindowGLFW {
-
-    private:
-        GLFWwindow* m_window = nullptr;
-
-    public:
-        WindowGLFW(const char* const title, int winWidth, int winHeight, bool fullscreen) {
-            glfwSetErrorCallback(callback_error);
-
-            if (GLFW_FALSE == glfwInit())
-                throw std::runtime_error("failed to initialize GLFW");
-
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-            this->m_window = glfwCreateWindow(winWidth, winHeight, title, NULL, NULL);
-            if (nullptr == this->m_window)
-                throw std::runtime_error("failed to create window");
-
-            glfwSetFramebufferSizeCallback(this->m_window, ::callback_resize_fbuf);
-
-            glfwSetWindowSizeLimits(this->m_window, MIN_WIN_WIDTH, MIN_WIN_HEIGHT, GLFW_DONT_CARE, GLFW_DONT_CARE);
-            glfwMakeContextCurrent(this->m_window);
-            if (0 == gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-                throw std::runtime_error("failed to load OpenGL functions");
-            }
-            glfwSwapInterval(1);
-        }
-
-        ~WindowGLFW(void) {
-            glfwDestroyWindow(this->m_window);
-            this->m_window = nullptr;
-            glfwTerminate();
-        }
-
-        void poll_events() {
-            glfwPollEvents();
-        }
-
-        void swap_buffers(void) {
-            glfwSwapBuffers(this->m_window);
-        }
-
-        bool should_close(void) {
-            return glfwWindowShouldClose(this->m_window);
-        }
-
-        auto get_window_size(void) {
-            int w, h;
-            glfwGetFramebufferSize(this->m_window, &w, &h);
-            return std::make_pair(w, h);
-        }
-
-        auto get_mouse_pos(void) {
-            double xpos, ypos;
-            glfwGetCursorPos(this->m_window, &xpos, &ypos);
-            return std::make_pair(xpos, ypos);
-        }
-
-    };
-
-}
-
-
-// OpenGL
-namespace {
-
-    enum class ShaderType{ vertex, fragment };
-
-
-    class ShaderRAII {
-
-    private:
-        GLuint m_id;
-
-    public:
-        ShaderRAII(const ::ShaderType type, const char* const src) {
-            this->init(type, src);
-        }
-
-        ~ShaderRAII(void) {
-            this->destroy();
-        }
-
-        void init(const ::ShaderType type, const char* const src) {
-            this->destroy();
-
-            switch (type) {
-                case ::ShaderType::vertex:
-                    this->m_id = glCreateShader(GL_VERTEX_SHADER);
-                    break;
-                case ::ShaderType::fragment:
-                    this->m_id = glCreateShader(GL_FRAGMENT_SHADER);
-                    break;
-            }
-
-            assert(0 != this->m_id);
-
-            glShaderSource(this->m_id, 1, &src, nullptr);
-            glCompileShader(this->m_id);
-
-            GLint shader_compiled = GL_FALSE;
-            glGetShaderiv(this->m_id, GL_COMPILE_STATUS, &shader_compiled);
-            if (GL_TRUE != shader_compiled) {
-                constexpr auto SHADER_COMPILER_LOG_BUF_SIZE = 2048;
-                GLsizei length = 0;
-                char log[SHADER_COMPILER_LOG_BUF_SIZE];
-                glGetShaderInfoLog(this->m_id, SHADER_COMPILER_LOG_BUF_SIZE, &length, log);
-                throw std::runtime_error(log);
-            }
-        }
-
-        void destroy() {
-            if (this->is_ready()) {
-                glDeleteShader(this->m_id);
-                this->m_id = 0;
-            }
-        }
-
-        bool is_ready() const {
-            return 0 != this->m_id;
-        }
-
-        GLuint get() const {
-            return this->m_id;
-        }
-
-    };
-
-
-    class ShaderProgram {
-
-    private:
-        GLuint m_program = 0;
-
-    public:
-        ShaderProgram() = default;
-
-        ShaderProgram(const char* const vert_src, const char* const frag_src) {
-            this->init(vert_src, frag_src);
-        }
-
-        ~ShaderProgram() {
-            this->destroy();
-        }
-
-        void init(const char* const vert_src, const char* const frag_src) {
-            this->destroy();
-            this->m_program = glCreateProgram();
-            assert(0 != this->m_program);
-
-            const ::ShaderRAII vert_shader(::ShaderType::vertex, vert_src);
-            const ::ShaderRAII frag_shader(::ShaderType::fragment, frag_src);
-
-            glAttachShader(this->m_program, vert_shader.get());
-            glAttachShader(this->m_program, frag_shader.get());
-            glLinkProgram(this->m_program);
-
-            GLint success = GL_TRUE;
-            glGetProgramiv(this->m_program, GL_LINK_STATUS, &success);
-            if (GL_TRUE != success) {
-                GLsizei length = 0;
-                char log[100];
-                glGetProgramInfoLog(this->m_program, 100, &length, log);
-                throw std::runtime_error(log);
-            }
-        }
-
-        void destroy() {
-            if (this->is_ready()) {
-                glDeleteProgram(this->m_program);
-                this->m_program = 0;
-            }
-        }
-
-        bool is_ready() const {
-            return 0 != this->m_program;
-        }
-
-        void use() const {
-            glUseProgram(this->m_program);
-        }
-
-    };
-
-
-    class TextureGL {
-
-    private:
-        GLuint m_tex = 0;
-
-    public:
-        void init() {
-            this->destroy();
-
-            glGenTextures(1, &this->m_tex);
-            assert(0 != this->m_tex);
-            glBindTexture(GL_TEXTURE_2D, this->m_tex);
-//*/
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-/*/
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//*/
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
-
-        void destroy() {
-            if (this->is_ready()) {
-                glDeleteTextures(1, &this->m_tex);
-                this->m_tex = 0;
-            }
-        }
-
-        void update(const moonrock::ImageUint2D& image) {
-            assert(this->is_ready());
-
-            glBindTexture(GL_TEXTURE_2D, this->m_tex);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
-            glGenerateMipmap(GL_TEXTURE_2D);
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
-
-        bool is_ready() const {
-            return 0 != this->m_tex;
-        }
-
-        auto get() const {
-            return this->m_tex;
-        }
-
-    };
-
-}
+#include <SDL.h>
 
 
 namespace {
 
-    class RenderMaster {
+    class WindowSDL {
 
     private:
-        ::WindowGLFW m_window;
-        ::ShaderProgram m_shader;
-        ::TextureGL m_texture;
+        SDL_Window* m_window = nullptr;
+        SDL_Surface* m_surface = nullptr;
 
     public:
-        RenderMaster()
-            : m_window("Moonrock renderer", 800, 800, false)
-        {
-            const auto resource_folder = moonrock::find_parent_folder_containing_folder_named("resource") + "/resource";
+        ~WindowSDL() {
+            this->destroy();
+        }
 
-            this->m_shader.init(
-                load_str_from_disk(resource_folder + "/fill_screen.vert").c_str(),
-                load_str_from_disk(resource_folder + "/fill_screen.frag").c_str()
+        bool init(
+            const char* const title,
+            const unsigned window_width,
+            const unsigned window_height
+        ) {
+            if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                return false;
+            }
+
+            this->m_window = SDL_CreateWindow(
+                title,
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                static_cast<int>(window_width),
+                static_cast<int>(window_height),
+                SDL_WINDOW_SHOWN
             );
-
-            this->m_texture.init();
-
-            glClearColor(0, 0, 0, 1);
-        }
-
-        bool update() {
-            this->m_window.poll_events();
-
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            if (this->m_texture.is_ready()) {
-                this->m_shader.use();
-                glActiveTexture(GL_TEXTURE0);
-                glBindTexture(GL_TEXTURE_2D, this->m_texture.get());
-                glDrawArrays(GL_TRIANGLES, 0, 3);
+            if (nullptr == this->m_window) {
+                return false;
             }
 
-            this->m_window.swap_buffers();
-            glFlush();
-            return !this->m_window.should_close();
+            this->m_surface = SDL_GetWindowSurface(this->m_window);
+
+            return true;
         }
 
-        void set_image(const moonrock::ImageUint2D& image) {
-            this->m_texture.update(image);
+        void destroy() {
+            if (nullptr != this->m_window) {
+                SDL_DestroyWindow(this->m_window);
+                SDL_Quit();
+
+                this->m_window = nullptr;
+                this->m_surface = nullptr;
+            }
+        }
+
+        void update_surface() {
+            SDL_UpdateWindowSurface(this->m_window);
         }
 
     };
@@ -315,19 +59,11 @@ namespace {
 }
 
 
-int main() {
-    ::RenderMaster renderer;
-    RenderThings render_things;
-    moonrock::Timer timer;
+int main(int argc, char* args[]) {
+    ::WindowSDL window;
+    window.init("Output Display", 800, 450);
 
-    do {
-        if (timer.elapsed() > 1) {
-            render_things.render();
-            renderer.set_image(render_things.m_fbuf.m_color_buffer);
-            timer.check();
-        }
-
-    } while (renderer.update());
+    SDL_Delay( 2000 );
 
     return 0;
 }
